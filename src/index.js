1466a9dae9baeeea394ba3ff44ab373f6af8f602
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-12 14:40:34 -0400

    Start implementing the home page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,23 @@ import getData from './modules/homePage.js';
 const mylogo = new Image();
 mylogo.src = Icon;
 
+const main = document.querySelector('main');
+
 const logo = document.querySelector('.logo');
 logo.appendChild(mylogo);
 
+const display = async () => {
+ const meals = await getData('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
+ console.log(meals);
+ meals.forEach(({strMeal, strMealThumb}) => {
+  console.log(strMealThumb);
+  const article = `
+  <article>
+   <div class="meal_img"><img src="${strMealThumb}" alt=""></div>
+   <div class="title"><h3>${strMeal}</h3><span><i class="fas fa-thumbs-up"></i></span></div>
+   <button class="btn_comment">Comments</button>
+  </article>`;
+  main.insertAdjacentHTML('beforeend', article);
+ });
+};
+display();
--- src/modules/homePage.js
+++ src/modules/homePage.js
@@ -1,6 +1,6 @@
 const getData = async (url) => {
   const response = await fetch(url);
   const json = await response.json();
-  console.log(json);
+  return json.meals;
 }
 export default getData;
