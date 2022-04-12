8cd2c8f4d7b2f46885e6c7a48bc86147c8a551fa
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-12 12:07:46 -0400

    Add header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import Icon from './assest/logo1.png';
+ 
+const mylogo = new Image();
+mylogo.src = Icon;
+
+const logo = document.querySelector('.logo');
+logo.appendChild(mylogo);
--- webpack.config.js
+++ webpack.config.js
@@ -33,24 +33,15 @@ module.exports = {
   },
 
   module: {
-
     rules: [
-
       {
-
         test: /\.css$/i,
-
         use: ['style-loader', 'css-loader'],
-
       },
       {
-
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
         type: 'asset/resource',
-
       },
-
     ],
 
   },
