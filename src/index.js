6ff6359b839ad180f2339297a3db1779cb60d5f1
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-14 09:06:55 -0400

    Implement like function and style like button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import Icon from './assest/logo1.png';
 import getData from './modules/homePage.js';
+import Likes from './modules/Likes.js';
  
 const mylogo = new Image();
 mylogo.src = Icon;
@@ -12,14 +13,60 @@ logo.appendChild(mylogo);
 
 const display = async () => {
  const meals = await getData('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
- meals.forEach(({strMeal, strMealThumb}) => {
+ let liked = [];
+ if (localStorage.getItem('liked') != null) {
+  const lik = JSON.parse(localStorage.getItem('liked'));
+  liked = lik.map((item) => item.item_id);
+ }
+
+ meals.forEach(({idMeal, strMeal, strCategory, strArea, strInstructions, strMealThumb}) => {
   const article = `
   <article>
    <div class="meal_img"><img src="${strMealThumb}" alt=""></div>
-   <div class="title"><h3>${strMeal}</h3><span><i class="fas fa-thumbs-up"></i></span></div>
+   <div class="title"><h3>${strMeal}</h3>
+   <div class="like_box"><span class="nbr_like"></span><span style=" color: ${liked.includes('item_' + idMeal) ? '#ed1c24' : ''}" liked="${liked.includes('item_' + idMeal) ? 'true' : 'false'}" class="fas fa-thumbs-up" id="item_${idMeal}"></span></div>
+   </div>
+   <p class="inst">${strInstructions.slice(0,100)}....</p>
+   <p class="CA"><span><strong>Category:</strong> ${strCategory}</span><span><strong>Area:</strong> ${strArea}</span></p>
    <button class="btn_comment">Comments</button>
+   
   </article>`;
   container.insertAdjacentHTML('beforeend', article);
  });
 };
-display();
+
+const ftnLikes = async () => {
+ const like_icon = document.querySelectorAll('.fa-thumbs-up');
+ like_icon.forEach(element => {
+  element.addEventListener('click', async (e) => {
+   const nbrLike = e.target.previousSibling;
+   const likeIcon = e.target;
+   let i = +nbrLike.textContent;
+   const sdLink = new Likes(likeIcon.id);
+   const check = await sdLink.postLikes();
+   if (check) {
+    i++;
+    nbrLike.innerHTML = i;
+   }
+
+   if (likeIcon.getAttribute('liked') === 'false') {
+    likeIcon.style.color = '#ed1c24';
+    likeIcon.setAttribute('liked', 'true');
+   } else if((likeIcon.getAttribute('liked') === 'true')) {
+    likeIcon.style.color = '';
+    likeIcon.setAttribute('liked', 'false');
+   }
+  });
+ });
+}; 
+display().then(ftnLikes);
+
+const dplLikes = async () => {
+ const sdLink = new Likes();
+ sdLink.getLikes().then((value) => {
+  value.forEach(({likes, item_id}) => {
+   document.getElementById(item_id).previousSibling.textContent = likes;
+  });
+ });
+}
+dplLikes();
---
+++ src/modules/Likes.js
@@ -0,0 +1,58 @@
+export default class Likes {
+ constructor(item_id = '', url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/O7NaeKDfq7oZ1XEIy2sV/likes/') {
+  this.item_id = item_id;
+  this.url = url;
+  this.liked = localStorage.getItem('liked') != null
+        ? JSON.parse(localStorage.getItem('liked')) 
+        : '';
+  this.data = {
+   "item_id": this.item_id,
+ };
+ }
+
+ saveLiked(TSave = this.liked) {
+  const storeLiked = JSON.stringify(TSave);
+  localStorage.setItem('liked', storeLiked);
+}
+
+ async getLikes() {
+  const response = await fetch(this.url);
+  const json = await response.json();
+  return json;
+ }
+
+ async postLikes() {
+  const option = {
+   method: 'POST',
+   headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+  body: JSON.stringify(this.data),
+  };
+
+  if (this.liked === '' || this.liked.lenght === 0) {
+    const response = await fetch(this.url, option);
+    response.then(() => {
+      this.liked = [this.data];
+      this.saveLiked();
+    });
+    this.liked = [this.data];
+    this.saveLiked();
+    return true;
+  } else {
+    const likedSort = this.liked.map((item) => item.item_id);
+    if (likedSort.includes(this.item_id)) {
+      return false;
+    }else {
+      const response = await fetch(this.url, option);
+      response.then(() => {
+        this.liked.push(this.data);
+        this.saveLiked();
+      });
+      this.liked.push(this.data);
+      this.saveLiked();
+      return true;
+    }
+  }
+ };
+}
