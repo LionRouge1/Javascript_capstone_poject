e8c52706b128f03ab0df392f492362a3934d4eaf
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-15 17:54:45 -0400

    Test function calculeLike

---
+++ src/modules/__mocks__/Likes.js
@@ -0,0 +1,12 @@
+export default class Likes {
+ constructor(itemId, url = '') {
+  this.itemId = itemId;
+  this.url = url;
+ }
+
+ async postLikes() {
+  if(this.itemId >= 0) {
+   return true;
+  }
+   };
+}
