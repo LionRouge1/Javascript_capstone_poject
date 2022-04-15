a0fd9e7577df371fd23cf13ca505e4b94450b45e
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-15 19:08:40 -0400

    Correct linter errors

--- src/modules/__mocks__/Likes.js
+++ src/modules/__mocks__/Likes.js
@@ -1,12 +1,13 @@
 export default class Likes {
- constructor(itemId, url = '') {
-  this.itemId = itemId;
-  this.url = url;
- }
-
- async postLikes() {
-  if(this.itemId >= 0) {
-   return true;
+  constructor(itemId, url = '') {
+    this.itemId = itemId;
+    this.url = url;
   }
-   };
+
+  async postLikes() {
+    if (this.itemId >= 0) {
+      return true;
+    }
+    return false;
+  }
 }
